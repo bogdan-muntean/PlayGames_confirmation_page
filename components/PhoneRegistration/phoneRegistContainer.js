60346d00f729ba9741b2c2b6c393d2fbc4c5eb9b
bogdan-muntean <https://github.com/bogdan-muntean>
2023-11-11 14:33:13 +0200

    added responsiveness for all project

--- components/PhoneRegistration/phoneRegistContainer.js
+++ components/PhoneRegistration/phoneRegistContainer.js
@@ -9,11 +9,29 @@ export default function showPhoneRegistContainer () {
                 Receive an access code by SMS <br>
                 <span>Enter your phone number</span>
             </div>
-            <div class="rectangle">
-                <img src="./assets/phone.svg">
-                <div>
+            <div class="rectangle-phone-input">
+                <div class="rectangle-phone-input-left-content">
+                    <img src="./assets/phone.svg">
+                    <input type="text" id="phoneNumber" value="+40">
                 </div>
             </div>
+            <button onclick="validatePhoneNumber()" id="continueBtn">
+                CONTINUE
+            </button>
         </div>
     `;
+}
+
+function validatePhoneNumber() {
+    // Get the phone number value
+    let phoneNumber = document.querySelector("#phoneNumber").value;
+
+    // Define a regular expression pattern for the phone number
+    let pattern = /^\+40\d{9}$/;
+    // Check if the phone number matches the pattern
+    if (pattern.test(phoneNumber)) {
+        document.getElementById("validationResult").innerText = "Phone number is valid.";
+    } else {
+        document.getElementById("validationResult").innerText = "Invalid phone number format. It should be in the pattern +40xxxxxxxxx.";
+    }
 }
